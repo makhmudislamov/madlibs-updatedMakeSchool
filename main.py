602cc42bb05ee5654c29c95d02f4835caf311f0e
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-30 21:27:28 -0700

    using main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from matrix import matrix_theme
+# from hpotter import hpotter_theme
 
 def user_input(prompt):
     # the input function will display a message in the terminal
@@ -9,7 +10,11 @@ def user_input(prompt):
 def start_madlibs():
 
     theme_choice = user_input("choose M or H")
-    if theme_choice == "M":
+    if theme_choice in ["M", "m"]:
         matrix_theme()
+    # elif theme_choice in ["H","h"]:
+    #     hpotter_theme()
+    # else:
+    #     print("invalid input")
 
 start_madlibs()
